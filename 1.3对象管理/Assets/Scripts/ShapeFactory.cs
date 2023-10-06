b22c1f12fb1eb32208f36067ca28f8e39fd48ec3
xuhang <38711680+xuhang123456a@users.noreply.github.com>
2023-10-06 13:50:34 +0800

    第三篇：复用对象（Object Pools）

--- 1.3对象管理/Assets/Scripts/ShapeFactory.cs
+++ 1.3对象管理/Assets/Scripts/ShapeFactory.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu]
 public class ShapeFactory : ScriptableObject
@@ -9,6 +10,7 @@ public class ShapeFactory : ScriptableObject
     [SerializeField] private bool recycle;
 
     private List<Shape>[] pools;
+    private Scene poolScene;
 
     public Shape Get(int shapeId = 0, int materialId = 0)
     {
@@ -29,6 +31,7 @@ public class ShapeFactory : ScriptableObject
             {
                 instance = Instantiate(prefabs[shapeId]);
                 instance.ShapeId = shapeId;
+                SceneManager.MoveGameObjectToScene(instance.gameObject, poolScene);
             }
         }
         else
@@ -53,6 +56,26 @@ public class ShapeFactory : ScriptableObject
         {
             pools[i] = new List<Shape>();
         }
+
+        if (Application.isEditor)
+        {
+            poolScene = SceneManager.GetSceneByName(name);
+            if (poolScene.isLoaded)
+            {
+                GameObject[] rootObjects = poolScene.GetRootGameObjects();
+                for (int i = 0; i < rootObjects.Length; i++)
+                {
+                    Shape pooledShape = rootObjects[i].GetComponent<Shape>();
+                    if (pooledShape.gameObject.activeSelf)
+                    {
+                        pools[pooledShape.ShapeId].Add(pooledShape);
+                    }
+                }
+                return;
+            }
+        }
+
+        poolScene = SceneManager.CreateScene(name);
     }
 
     public void Reclaim(Shape shapeToRecycle)
@@ -63,6 +86,7 @@ public class ShapeFactory : ScriptableObject
             {
                 CreatePools();
             }
+
             pools[shapeToRecycle.ShapeId].Add(shapeToRecycle);
             shapeToRecycle.gameObject.SetActive(false);
         }
